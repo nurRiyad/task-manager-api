0583fd2040d53a790f07833820beebf70ebc110d
Al Asad Nur Riyad <45213090+nurRiyad@users.noreply.github.com>
2022-03-26 14:58:41 +0600

    Update mongodb database in (#1)
    Signed-off-by: Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 
 //Connect to the mongodb database
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager");
