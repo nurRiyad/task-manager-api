83260fc367e3a648d7b76497dacd59ee49ed1a63
Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>
2022-04-20 22:28:19 +0600

    Add a sib-api-v3-sdk mocks module for testing
    Signed-off-by: Md. Al Asad Nur Riyad <asadnurriyad@gmail.com>

---
+++ tests/__mocks__/sib-api-v3-sdk.js
@@ -0,0 +1,14 @@
+module.exports = {
+  TransactionalEmailsApi: function () {
+    this.sendTransacEmail = function () {};
+  },
+  ApiClient: {
+    instance: {
+      authentications: {
+        "api-key": {
+          apiKey: "",
+        },
+      },
+    },
+  },
+};
